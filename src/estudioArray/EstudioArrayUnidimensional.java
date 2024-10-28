b3a209d8fd899d1a48afef2797a9644007e583bb
Egararj <egararj@gmail.com>
2024-10-28 10:13:56 +0100

    a

--- src/estudioArray/EstudioArrayUnidimensional.java
+++ src/estudioArray/EstudioArrayUnidimensional.java
@@ -17,15 +17,27 @@ public class EstudioArrayUnidimensional {
         }
         System.out.println("Han suspendido "+suspenso+" alumnos");
         int[] notas2=notas; //notas2 y notas comparten el mismo espacio en la memoria. Son el mismo pero con distinto nombre, no es una copia aparte
-        double media=calculaMedia(notas);
+        int numero=5;
+        double media=calculaMedia(notas, numero);
+        int[] primitiva=generaPrimitiva();
         System.out.println("pausa");
     }
 
-    private double calculaMedia(int[] notasAsignatura) {//si los valores de notasAsignaturas cambian lo hará también notas, ojo con eso
+    private int[] generaPrimitiva() {
+        int[] primitiva = new int[6];
+        for(int i=0;i<primitiva.length;i++){
+            
+        }
+
+        return primitiva;
+    }
+
+    private double calculaMedia(int[] notasAsignatura, int numeroDePrueba) {//si los valores de notasAsignaturas cambian lo hará también notas, ojo con eso
         double media=0;
         for(int i=0;i<notasAsignatura.length;i++){
             media+=notasAsignatura[i];
         }
+        numeroDePrueba=3;
         return media/notasAsignatura.length;
     }
 

818f21cc838210c40f4d3ac31f862402dd1f655a
Egararj <egararj@gmail.com>
2024-11-08 12:32:52 +0100

    boletin de notas

--- src/estudioArray/Notas.java
+++ src/estudioArray/Notas.java
@@ -10,17 +10,14 @@ public class Notas {
 
         int[][]notas={
             {5,6,7,5,7,3,4,6,8,1},
-            {2,4,5,3,7,6,8,4,9,10},
-            {1,2,3,4,5,6,7,8,9,10},
+            {8,4,5,3,7,6,8,4,9,10},
+            {7,2,3,4,5,6,7,8,9,10},
             {5,4,3,2,1,5,4,3,2,1}
         };
         String [] alumnos={"Juan","Felipe","Ana","Maria","Antonio","Rosa","Pedro","Carmen","Pablo","Luis"};
         String [] asignaturas={"Programacion","Sistemas","BBDD","Entornos"};
-        System.out.println("Mostrar notas de una asignatura");
-        System.out.println("1.Programacion");
-        System.out.println("2.Sistemas");
-        System.out.println("3.BBDD");
-        System.out.println("4.Entornos");
+        System.out.println("1.Mostrar notas de una asignatura");
+        System.out.println("2.Mostrar boletín de un alumno con su nota media si ha aprobado");
 
         System.out.println("Elija una opción");
         int opcion=0;
@@ -29,27 +26,55 @@ public class Notas {
         leer=null;
         switch (opcion) {
             case 1:
-                mostrarAsignatura(opcion, notas, alumnos, asignaturas);
+                System.out.println("1.Programacion-2.Sistemas-3.BBDD-4.Entornos");
+                leer= new Scanner(System.in);
+                int asignatura=leer.nextInt();
+                leer=null;
+                asignatura--;
+                mostrarAsignatura(asignatura, notas, alumnos, asignaturas);
             break;
             case 2:
-                mostrarAsignatura(opcion, notas, alumnos, asignaturas);
-                break;
+                System.out.println("1.Juan-2.Felipe-3.Ana-4.Maria-5.Antonio-6.Rosa-7.Pedrp-8.Carmen-9.Pablo-10.Luis");
+                leer= new Scanner(System.in);
+                int alumno=leer.nextInt();
+                leer=null;
+                alumno--;
+                mostrarBoletin(alumno, notas, alumnos, asignaturas);
+                               
+            break;
             case 3:
-                mostrarAsignatura(opcion, notas, alumnos, asignaturas);              
-                break;
+                                          
+            break;
             case 4:
-                mostrarAsignatura(opcion, notas, alumnos, asignaturas);  
+
+            break;                 
             default:
-                break;
-             }
-                
+            break;
+            }
+                                
+    }
+                                
+    private void mostrarBoletin(int alumno, int[][] notas, String[] alumnos, String[] asignaturas) {
+    System.out.println("Boletín de "+alumnos[alumno]);
+    int media=0;
+    boolean aprobado=true;
+
+    for(int c=alumno, f=0; f<notas.length; f++){
+        System.out.println(asignaturas[f]+" : "+notas[f][c]);
+        media+=notas[f][c];
+        if(notas[f][c]<5)
+        aprobado=false;
+    }
+    if(aprobado)
+    System.out.println("Nota media : "+media/4);
+    return;
     }
                 
-    private void mostrarAsignatura(int opcion, int[][] notas, String[] alumnos, String[] asignaturas) {
-        System.out.println("Notas "+asignaturas[opcion-1]);
-        for (int i=0; i<notas[opcion-1].length; i++){
-            System.out.printf("Alumno: %-8s. Nota: %d %n",alumnos[i], notas[opcion-1][i]);
-        }
-        return;
+    private void mostrarAsignatura(int asignatura, int[][] notas, String[] alumnos, String[] asignaturas) {
+    System.out.println("Notas "+asignaturas[asignatura]);
+    for (int c=0; c<notas[asignatura].length; c++){
+        System.out.printf("Alumno: %-8s. Nota: %d %n",alumnos[c], notas[asignatura][c]);
+    }
+    return;
     }
 }

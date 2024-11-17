933aeee9a7293833c8742ae5844a1df74ff453bd
Egararj <egararj@gmail.com>
2024-11-17 10:02:55 +0100

    primitiva

---
+++ src/estudioArray/Primitiva.java
@@ -0,0 +1,74 @@
+package estudioArray;
+
+import java.util.Arrays;
+
+public class Primitiva {
+    public Primitiva(){
+        System.out.println("Sorteo para 10 boletos de primitiva");
+
+        //Genera 10 boletos
+        int [][] boletos=new int [10][6];
+        for (int f=0; f<boletos.length; f++){
+            boletos[f]=generadorBoleto();
+        }
+
+        //Genera el premio
+        int[] premio=generadorBoleto();
+
+        //Comparación
+        for (int f=0, aciertos=0; f<boletos.length; f++){
+            aciertos=0;
+            for (int c=0; c<boletos[f].length; c++){
+                for (int c1=0; c1<premio.length; c1++){
+                    if (boletos[f][c]==premio[c1]) aciertos++;
+                }
+            }
+            System.out.println("El boleto "+f+" tiene "+aciertos+" aciertos");
+        }
+
+        System.out.println("Genera boletos hasta que coincida con el boleto premiado");
+
+        int[] premio2=generadorBoleto();
+        int[] boletoGanador;
+        long totalBoletos=0;
+        boolean Iguales=true;
+
+        do{
+            boletoGanador=generadorBoleto();
+            Iguales=Arrays.equals(premio2, boletoGanador);
+            totalBoletos++;
+        }
+        while(!Iguales);
+        System.out.println("El premio es:");
+        System.out.println(Arrays.toString(premio2));
+        System.out.println("Han hecho falta generar "+totalBoletos+" boletos para acertar");
+        
+
+        System.out.println("pausa");
+        }
+        
+        private int[] generadorBoleto() {
+            int[] boleto=new int [6];
+            int numero=0;
+            boolean repetido=false;
+           
+            for (int c=0; c<boleto.length; c++){
+                repetido=true;
+                while (repetido){
+                    numero=(int) Math.floor(Math.random()*49+1);
+                    for (int c1=0; c1<boleto.length; c1++){
+                        if (numero==boleto[c1]){
+                        repetido=true;
+                        break;
+                        }
+                        else repetido=false;
+                    }
+                }
+                boleto[c]=numero;
+            }
+            Arrays.sort(boleto);
+            return boleto;
+        }
+
+
+}
--- src/vista/Main.java
+++ src/vista/Main.java
@@ -4,13 +4,15 @@ import estudioArray.EjerciciosArray;
 import estudioArray.EstudioArrayBidimiensionales;
 import estudioArray.EstudioArrayUnidimensional;
 import estudioArray.Notas;
+import estudioArray.Primitiva;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         //EstudioArrayUnidimensional ejercicio=new EstudioArrayUnidimensional();
         //EstudioArrayBidimiensionales ejercicio=new EstudioArrayBidimiensionales();
         //Notas ejercicio=new Notas();
-        EjerciciosArray ejercicio=new EjerciciosArray();
+        //EjerciciosArray ejercicio=new EjerciciosArray();
+        Primitiva ejercicio=new Primitiva();
 
     }
 }

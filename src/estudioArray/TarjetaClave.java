ff11205b71040ecd0dacba420db38c993eaa630a
Egararj <egararj@gmail.com>
2024-11-29 12:02:54 +0100

    a

--- src/estudioArray/TarjetaClave.java
+++ src/estudioArray/TarjetaClave.java
@@ -12,15 +12,16 @@ public class TarjetaClave {
         int [][] tarjeta=tarjetaClave();
 
         //Imprime la tarjeta
-        System.out.println("    A    B    C    D    E");
+        System.out.println("    A    B    C    D    E    F");
         for (int f=0; f<tarjeta.length; f++){
             System.out.printf((f+1)+" "+(Arrays.toString(tarjeta[f])));
             System.out.println();
         }
-
+        //Obtiene la clave a acertar
         int clave=0;
         clave = posicionAleatoria(clave, tarjeta);
         
+        //Intentos para acertar la clave
         boolean acierto=false;
         for (int x=3, intento=0; x>0; x--){
             System.out.println("Le quedan "+x+" intentos. Intruduzca la clave");
@@ -45,7 +46,7 @@ public class TarjetaClave {
                 
             private int posicionAleatoria(int clave, int[][] tarjeta) {
                 int f= (int) Math.floor(Math.random()*5);
-                int c= (int) Math.floor(Math.random()*5);
+                int c= (int) Math.floor(Math.random()*6);
                 String columnaLetra="ABCDE";
                 clave=tarjeta[f][c];
                 System.out.println("La posición que tiene que acertar es: "+columnaLetra.charAt(c)+"-"+(f+1));
@@ -54,28 +55,28 @@ public class TarjetaClave {
             }
         
             private int[][] tarjetaClave() {
-                int [][] tarjeta= new int [5][5];
-                int[] ale25= new int [25];
+                int [][] tarjeta= new int [5][6];
+                int[] ale30= new int [30];
                 boolean iguales=false;
                 int ale=0;
         
-                for (int x=0; x<25;x++){
+                for (int x=0; x<30;x++){
                     do{
                         iguales=false;
                         ale=(int) Math.floor(Math.random()*(999-100+1)+100);
                         for (int y=0; y<=x; y++){
-                            if(ale==ale25[y]) {
+                            if(ale==ale30[y]) {
                                 iguales=true;
                                 break;
                             }
                         }
                     } while(iguales);
-                    ale25[x]=ale;
+                    ale30[x]=ale;
                 }    
 
                 for (int f=0, x=0; f<tarjeta.length; f++){
                     for (int c=0; c<tarjeta[f].length; c++, x++){
-                        tarjeta[f][c]=ale25[x];
+                        tarjeta[f][c]=ale30[x];
                     }
                 }
             return tarjeta;
